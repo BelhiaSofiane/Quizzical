8bb7f7ce0ab682034980d12a2c5eeb885b5539e8
sofiane <sofiane14008@gmail.com>
2024-10-01 18:01:22 +0100

    added css

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -2,38 +2,42 @@ import React from "react";
 
 const Questions = ({ qna = [], handleClickAnswer, count, handleCheckAnswers }) => {
   const questionsElement = qna.map((question, index) => (
-    <div key={index}>
+    <div key={index} className="question-container">
       {/* Decode and display the question */}
       <h1 className="question-title">{atob(question.question)}</h1>
 
       {/* Map through the answers for each question */}
-      {question.answers.map((answer, i) => (
-        <button
-          key={i}
-          onClick={() => handleClickAnswer(question.id, answer)}
-          className={
-            question.checked 
-              ? answer === question.correct 
-                ?'correct'
-                :question.selectedAnswer === answer 
-                  ?'incorrect'
-                  :''
-              : question.selectedAnswer === answer 
-                ?'selected'
-                : 'not-selected'
-          }
-        >
-          {atob(answer)}
-        </button>
-      ))}
+      <div className="answer-container">
+        {question.answers.map((answer, i) => (
+          <button
+            key={i}
+            onClick={() => handleClickAnswer(question.id, answer)}
+            className={
+              `${question.checked 
+                ? answer === question.correct 
+                  ?'correct'
+                  :question.selectedAnswer === answer 
+                    ?'incorrect'
+                    :''
+                : question.selectedAnswer === answer 
+                  ?'selected'
+                  :'not-selected'} `
+            }
+          >
+            {atob(answer)}
+          </button>
+        ))}
+      </div>
+      <br/>
     </div>
   ));
 
   return (
     <>
-      {questionsElement}
-      <button onClick={() => handleCheckAnswers()}>Check Answers</button>
-      {count = 0 ? <p>Hello </p> : <p>you have answered {count}/5 correctly </p>}
+      <div className="quizz-container">
+        {questionsElement}
+        <button className="primary-btn" onClick={() => handleCheckAnswers()}>Check Answers</button>
+      </div>
     </>
   );
 };
